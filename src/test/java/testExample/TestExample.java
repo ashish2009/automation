897a822ce89d8cea2035128c54cf528534fe1579
Ashish Kumar <ashishkumar@Ashish.local>
2019-07-18 12:49:37 +0530

    update

---
+++ src/test/java/testExample/TestExample.java
@@ -0,0 +1,13 @@
+package testExample;
+
+import org.testng.annotations.Test;
+
+public class TestExample {
+
+
+
+    @Test
+    public void verifyJobTrigger(){
+        System.out.println("Triggering Jenkins Job");
+    }
+}
